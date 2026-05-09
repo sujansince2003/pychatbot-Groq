9925c4e115256b9df16aa7c5bcbe223a7efd4438
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2026-05-10 00:41:50 +0545

    feat : defined the strreaming of the response generation to receive messages in chunks and added error handing for user choice

--- src/main.py
+++ src/main.py
@@ -11,19 +11,23 @@ load_dotenv()
 groq_client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 
+# defining function that calls the api with query and responds
 def respond_to_query(user_query):
     try:
         chat_response = groq_client.chat.completions.create(
             messages=[
                 {
                     "role": "system",
-                    "content": "you are chatbot which responds to user questions related to software development.response shouldnot be more than 50 words.",
+                    "content": "you are chatbot which responds to user questions related to software development.response shouldnot be more than 500 words.",
                 },
                 {"role": "user", "content": user_query},
             ],
             model="llama-3.3-70b-versatile",
+            stream=True,
         )
-        return chat_response.choices[0].message.content
+        # return chat_response.choices[0].message.content ::without streaming
+        return chat_response
+
     except:
         print("something went wrong.Try again later")
 
@@ -31,22 +35,29 @@ def respond_to_query(user_query):
 def main():
     while True:
         print("Enter 1 to ask questions and 2 to exit ")
-        user_choice = int(input("Enter your choice\n"))
+        user_choice = input("Enter your choice\n")
 
-        if type(user_choice) is not int:
-            print("Invalid choice,try again")
-        else:
-            if user_choice == 1:
+        # if type(user_choice) is not int:
+        #     print("Invalid choice,try again")
+        # else:
+        #     print("you can choose other")
+        try:
+            if user_choice == "1":
                 user_query = input("🤖 :what is your question \n")
                 if len(user_query) > 0:
                     response = respond_to_query(user_query)
-                    print(f"\n 🤖 : {response}\n")
-                else:
-                    print("sorry")
-            elif user_choice == 2:
+                    print(" 🤖 :")
+                    for chunk in response:
+                        # print(f"\n 🤖 : {response}\n")
+                        print(chunk.choices[0].delta.content, end="")
+                    else:
+                        print("sorry")
+            elif user_choice == "2":
                 break
             else:
-                print("invalid choice,try again")
+                raise ValueError("your choice is invalid!!!")
+        except ValueError as e:
+            print("⚠️ ", e, "\n")
 
 
 main()
