7f8c0b3b52bb7572dfc1bd605c4cf15304ad9c6a
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2026-05-10 00:13:09 +0545

    feat: defined the system role and sytem prompt for more reliability

--- src/main.py
+++ src/main.py
@@ -14,7 +14,13 @@ groq_client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 def respond_to_query(user_query):
     try:
         chat_response = groq_client.chat.completions.create(
-            messages=[{"role": "user", "content": user_query}],
+            messages=[
+                {
+                    "role": "system",
+                    "content": "you are chatbot which responds to user questions related to software development.response shouldnot be more than 50 words.",
+                },
+                {"role": "user", "content": user_query},
+            ],
             model="llama-3.3-70b-versatile",
         )
         return chat_response.choices[0].message.content
@@ -27,15 +33,20 @@ def main():
         print("Enter 1 to ask questions and 2 to exit ")
         user_choice = int(input("Enter your choice\n"))
 
-        if user_choice == 1:
-            user_query = input("🤖 :what is your question \n")
-            if len(user_query) > 0:
-                response = respond_to_query(user_query)
-                print(f"\n 🤖 : {response}\n")
-            else:
-                print("sorry")
+        if type(user_choice) is not int:
+            print("Invalid choice,try again")
         else:
-            break
+            if user_choice == 1:
+                user_query = input("🤖 :what is your question \n")
+                if len(user_query) > 0:
+                    response = respond_to_query(user_query)
+                    print(f"\n 🤖 : {response}\n")
+                else:
+                    print("sorry")
+            elif user_choice == 2:
+                break
+            else:
+                print("invalid choice,try again")
 
 
 main()
